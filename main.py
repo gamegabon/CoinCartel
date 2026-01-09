46fa11c233d0dbcc3f2b35b7339a297f16f0ae2a
gamegabon <parfaitahissou7@gmail.com>
2026-01-09 15:16:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -87,5 +87,6 @@ def run_bot():
 if __name__ == "__main__":
     threading.Thread(target=run_bot, daemon=True).start()
     port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)  
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
